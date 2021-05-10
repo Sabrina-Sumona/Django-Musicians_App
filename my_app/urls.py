c58705245260a807fd70c70556856a9a289eed53
Sabrina Sumona <snsbauet04@gmail.com>
2021-05-11 02:45:20 +0600

    CRUD using Django Queries

--- my_app/urls.py
+++ my_app/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('add_musician/', views.musician_form, name='musician_form'),
     path('album_list/', views.album_list, name='album_list'),
     path('musician_list/', views.musician_list, name='musician_list'),
+    path('musician_details/<int:artist_id>/', views.musician_details, name='musician_details'),
+    path('edit_artist/<int:artist_id>/', views.edit_artist, name='edit_artist'),
+    path('edit_album/<int:album_id>/', views.edit_album, name='edit_album'),
+    path('delete_album/<int:album_id>/', views.delete_album, name='delete_album'),
+    path('delete_artist/<int:artist_id>/', views.delete_musician, name='delete_artist'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -4,6 +4,9 @@ from my_app.models import Musician, Album
 
 from my_app.form import MusicianForm
 from my_app.form import AlbumForm
+
+from django.db.models import Avg
+
 # Create your views here.
 
 def index(request):
@@ -41,3 +44,49 @@ def album_form(request):
 
     diction = {'title':"Add Album" , 'text':'Add Album','album_form':AlbumForm()}
     return render(request, 'my_app/album_form.html',context=diction)
+
+def musician_details(request, artist_id):
+    artist_info = Musician.objects.get(pk=artist_id)
+    album_list = Album.objects.filter(artist=artist_id).order_by('name','release_date')
+    artist_rating = Album.objects.filter(artist=artist_id).aggregate(Avg('num_stars'))
+
+    diction = {'title':"Musician Details", 'artist_info':artist_info, 'album_list':album_list, 'artist_rating':artist_rating}
+    return render(request, 'my_app/musician_details.html', context=diction)
+
+def edit_artist(request, artist_id):
+    artist_info = Musician.objects.get(pk=artist_id)
+    form = MusicianForm(instance=artist_info)
+    if request.method == 'POST':
+        form = MusicianForm(request.POST, instance=artist_info)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return musician_details(request, artist_id)
+
+    diction = {'edit_form':form}
+    return render(request, 'my_app/edit_artist.html', context=diction)
+
+def edit_album(request, album_id):
+    album_info = Album.objects.get(pk=album_id)
+    form = AlbumForm(instance=album_info)
+    diction = {}
+    if request.method == 'POST':
+        form = AlbumForm(request.POST, instance=album_info)
+
+        if form.is_valid():
+            form.save(commit=True)
+            diction.update({'success_text':'Successfully Updated!'})
+
+    diction.update({'edit_form':form})
+    diction.update({'album_id':album_id})
+    return render(request,'my_app/edit_album.html', context=diction)
+
+def delete_album(request, album_id):
+    album = Album.objects.get(pk=album_id).delete()
+    diction = {'delete_success':'Album Deleted Successfully!'}
+    return render(request, 'my_app/delete.html', context=diction)
+
+def delete_musician(request, artist_id):
+    artist = Musician.objects.get(pk=artist_id).delete(0)
+    diction = {'delete_success':'Musician Deleted Successfully!'}
+    return render(request, 'my_app/delete.html', context=diction)
