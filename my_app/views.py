b269adf2eccf682290121ab44060e60847d74c44
Sabrina Sumona <snsbauet04@gmail.com>
2021-05-16 12:19:13 +0600

    Dynamic Context

--- my_app/views.py
+++ my_app/views.py
@@ -6,3 +6,9 @@ from django.views.generic import View,TemplateView
 
 class Index(TemplateView):
     template_name = 'my_app/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sample_text_1'] = 'Sample Text 1'
+        context['sample_text_2'] = 'Sample Text 2'
+        return context
