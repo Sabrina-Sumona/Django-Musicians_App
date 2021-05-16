bd06fe5cf74193bc13ac584363eb7a6e08269ca8
Sabrina Sumona <snsbauet04@gmail.com>
2021-05-16 18:02:37 +0600

    Update View & Delete View

--- my_app/urls.py
+++ my_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('musician_details/<pk>/', views.MusicianDetail.as_view(), name='musician_details'),
     path('add_musician/', views.AddMusician.as_view(), name='add_musician'),
+    path('musician_update/<pk>/', views.UpdateMusician.as_view(), name='musician_update'),
+     path('musician_delete/<pk>/', views.DeletMusician.as_view(), name='musician_delete'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import View, TemplateView, ListView, DetailView, CreateView
+from django.views.generic import View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from my_app.models import Musician, Album
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -20,3 +21,15 @@ class AddMusician(CreateView):
     model = Musician
     # django automatically creates this template_name, but also can give it to avoid any unexpectations
     template_name = 'my_app/musician_form.html'
+
+class UpdateMusician(UpdateView):
+    fields = ('first_name','last_name', 'instrument')
+    model = Musician
+    template_name = 'my_app/musician_form.html'
+
+class DeletMusician(DeleteView):
+    context_object_name = 'musician'
+    model = Musician
+    # if Successfully deleted
+    success_url = reverse_lazy("my_app:index")
+    template_name = 'my_app/delete_musician.html'
