d1ee9b1bedd41d782e7d4bd164f4b830cf0c63cf
Sabrina Sumona <snsbauet04@gmail.com>
2021-05-16 11:53:02 +0600

    Template View

--- my_app/views.py
+++ my_app/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import View
+from django.views.generic import View,TemplateView
 
 # Create your views here.
 
-class Index(View):
-    def get(self, request):
-        return HttpResponse("Hello World!!")
+class Index(TemplateView):
+    template_name = 'my_app/index.html'
